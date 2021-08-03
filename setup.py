fb1736b913f32a6614e41f3e8f6e112f1b5757ec
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2021-08-04 00:00:58 +0200

    * setup.py: Fix package metadata

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup (
 	name = 'ordo',
 	packages = ['ordo'],
-	version = '0.3.4',
+	version = '0.4.0.dev0',
 	description = 'A lightweight feedback tool for Jupyter',
-	author = 'Andre J. Michell',
-	author_email = 'andre.j.michell2@gmail.com',
-	url = 'https://github.com/nbgallery/ordo',
-	keywords = ['nbgallery', 'Jupyter', 'grading'],
-	package_data = {'ordo': ['ordo.js'],},
+	author = 'Andre J. Michell <andre.j.michell2@gmail.com>, Stefan Sobernig <stefan.sobernig@wu.ac.at>, Maxim Vidgof <maxim.vidgof@wu.ac.at>',
+	author_email = 'ordo@alice.wu.ac.at',
+	url = 'https://github.com/nm-wu/ordo',
+	keywords = ['nbgallery', 'Jupyter', 'tutorial'],
+	package_data = {'ordo': ['ordo.js', 'ordo.css'],},
 )
